057b8819ccde1dbeef18b5afb264eea50c4f2ba8
Vareja0 <netomiguelfadel@gmail.com>
2025-04-12 17:27:02 -0300

    add Docker

--- Database/sqlalchemy/database.py
+++ Database/sqlalchemy/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///database.db"
+DATABASE_URL = "mysql+pymysql://client:bancodedados@db:3306/cow8_db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
